33a005a5063339ff476833ce969cca0c0b67d546
Jeremy Cook <jeremy.cook@cloudacademy.com>
2018-07-25 21:14:14 +1200

    initial load for DevOps Adoption Playbook LP

---
+++ DevOps-Adoption-Playbook/TerraformTest/terraform/main.tf
@@ -0,0 +1,246 @@
+terraform {
+  backend "s3" {
+    bucket = "tfstate.bucketname.goes.here"
+    key    = "state.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+### Network
+
+# Fetch AZs in the current region
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "main" {
+  cidr_block = "${terraform.workspace == "prod" ? var.vpc_cidr_prod : var.vpc_cidr_staging}"
+
+  tags {
+    Name = "${terraform.workspace}"
+  }
+}
+
+# Create var.az_count private subnets, each in a different AZ
+resource "aws_subnet" "private" {
+  count             = "${var.az_count}"
+  cidr_block        = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  vpc_id            = "${aws_vpc.main.id}"
+}
+
+# Create var.az_count public subnets, each in a different AZ
+resource "aws_subnet" "public" {
+  count                   = "${var.az_count}"
+  cidr_block              = "${cidrsubnet(aws_vpc.main.cidr_block, 8, var.az_count + count.index)}"
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+  vpc_id                  = "${aws_vpc.main.id}"
+  map_public_ip_on_launch = true
+}
+
+# IGW for the public subnet
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+# Route the public subnet traffic through the IGW
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.main.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.gw.id}"
+}
+
+# Create a NAT gateway with an EIP for each private subnet to get internet connectivity
+resource "aws_eip" "gw" {
+  count      = "${var.az_count}"
+  vpc        = true
+  depends_on = ["aws_internet_gateway.gw"]
+}
+
+resource "aws_nat_gateway" "gw" {
+  count         = "${var.az_count}"
+  subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
+  allocation_id = "${element(aws_eip.gw.*.id, count.index)}"
+}
+
+# Create a new route table for the private subnets
+# And make it route non-local traffic through the NAT gateway to the internet
+resource "aws_route_table" "private" {
+  count  = "${var.az_count}"
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${element(aws_nat_gateway.gw.*.id, count.index)}"
+  }
+}
+
+# Explicitely associate the newly created route tables to the private subnets (so they don't default to the main route table)
+resource "aws_route_table_association" "private" {
+  count          = "${var.az_count}"
+  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
+}
+
+### Security
+
+# ALB Security group
+# This is the group you need to edit if you want to restrict access to your application
+resource "aws_security_group" "lb" {
+  name        = "tf-ecs-alb"
+  description = "controls access to the ALB"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Traffic to the ECS Cluster should only come from the ALB
+resource "aws_security_group" "ecs_tasks" {
+  name        = "${terraform.workspace}-tf-ecs-tasks"
+  description = "allow inbound access from the ALB only"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = "${var.app_port}"
+    to_port         = "${var.app_port}"
+    security_groups = ["${aws_security_group.lb.id}"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+### ALB
+
+resource "aws_alb" "main" {
+  name            = "${terraform.workspace}-tf-ecs"
+  subnets         = ["${aws_subnet.public.*.id}"]
+  security_groups = ["${aws_security_group.lb.id}"]
+}
+
+resource "aws_alb_target_group" "app_1" {
+  name        = "${terraform.workspace}-tf-ecs-1"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = "${aws_vpc.main.id}"
+  target_type = "ip"
+}
+
+# Redirect all traffic from the ALB to the target group
+resource "aws_alb_listener" "front_end_listner1" {
+  load_balancer_arn = "${aws_alb.main.id}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.app_1.id}"
+    type             = "forward"
+  }
+}
+
+resource "aws_alb_listener" "front_end_listner2" {
+  load_balancer_arn = "${aws_alb.main.id}"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "${var.ssl_certificate_arn}"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.app_1.id}"
+    type             = "forward"
+  }
+}
+
+### ECS
+
+resource "aws_ecs_cluster" "main" {
+  name = "${terraform.workspace}-tf-ecs-cluster"
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "${terraform.workspace}-app"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "${var.fargate_cpu}"
+  memory                   = "${var.fargate_memory}"
+
+  container_definitions = <<DEFINITION
+[
+  {
+    "cpu": ${var.fargate_cpu},
+    "image": "${var.app_image}",
+    "memory": ${var.fargate_memory},
+    "name": "${terraform.workspace}-app",
+    "networkMode": "awsvpc",
+    "portMappings": [
+      {
+        "containerPort": ${var.app_port},
+        "hostPort": ${var.app_port}
+      }
+    ]
+  }
+]
+DEFINITION
+}
+
+resource "aws_ecs_service" "service1" {
+  name            = "${terraform.workspace}-tf-ecs-service-1"
+  cluster         = "${aws_ecs_cluster.main.id}"
+  task_definition = "${aws_ecs_task_definition.app.arn}"
+  desired_count   = "${var.app_count}"
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups = ["${aws_security_group.ecs_tasks.id}"]
+    subnets         = ["${aws_subnet.private.*.id}"]
+  }
+
+  load_balancer {
+    target_group_arn = "${aws_alb_target_group.app_1.id}"
+    container_name   = "${terraform.workspace}-app"
+    container_port   = "${var.app_port}"
+  }
+
+  depends_on = [
+    "aws_alb_listener.front_end_listner1",
+    "aws_alb_listener.front_end_listner2",
+  ]
+}
+
+resource "aws_route53_record" "cname_route53_record" {
+  zone_id = "${var.r53_zoneid}"    # Replace with your zone ID
+  name    = "${var.dns_subdomain}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_alb.main.dns_name}"
+    zone_id                = "${aws_alb.main.zone_id}"
+    evaluate_target_health = true
+  }
+}
