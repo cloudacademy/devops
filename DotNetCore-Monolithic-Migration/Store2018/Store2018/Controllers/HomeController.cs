0ada05de0614b9e0a348a02e6b00fc1132564180
Jeremy Cook <jeremy.cook@cloudacademy.com>
2018-08-07 20:40:45 +1200

    refactored - add environment vars for microservice URLs

--- DotNetCore-Monolithic-Migration/Store2018/Store2018/Controllers/HomeController.cs
+++ DotNetCore-Monolithic-Migration/Store2018/Store2018/Controllers/HomeController.cs
@@ -12,12 +12,34 @@ namespace Store2018.Controllers
 {
     public class HomeController : Controller
     {
+        private static string ACCOUNT_SERVICE_API_BASE = Environment.GetEnvironmentVariable("ACCOUNT_SERVICE_API_BASE");
+        private static string INVENTORY_SERVICE_API_BASE = Environment.GetEnvironmentVariable("INVENTORY_SERVICE_API_BASE");
+        private static string SHOPPING_SERVICE_API_BASE = Environment.GetEnvironmentVariable("SHOPPING_SERVICE_API_BASE");
+
+        public HomeController()
+        {
+            if (ACCOUNT_SERVICE_API_BASE == null)
+            {
+                ACCOUNT_SERVICE_API_BASE = "http://localhost:5001/api";
+            }
+
+            if (INVENTORY_SERVICE_API_BASE == null)
+            {
+                INVENTORY_SERVICE_API_BASE = "http://localhost:5002/api";
+            }
+
+            if (SHOPPING_SERVICE_API_BASE == null)
+            {
+                SHOPPING_SERVICE_API_BASE = "http://localhost:5003/api";
+            }
+        }
+
         public async Task<IActionResult> Index()
         {
             var client = new HttpClient();
 
             var user = new Consumer();
-            HttpResponseMessage res1 = await client.GetAsync("http://localhost:5001/api/consumers/5");
+            HttpResponseMessage res1 = await client.GetAsync($"{ACCOUNT_SERVICE_API_BASE}/consumers/5");
             if (res1.IsSuccessStatusCode)
             {
                 var result = res1.Content.ReadAsStringAsync().Result;
@@ -25,7 +47,7 @@ namespace Store2018.Controllers
             }
                 
             var products = new List<Product>();
-            HttpResponseMessage res2 = await client.GetAsync("http://localhost:5002/api/products");
+            HttpResponseMessage res2 = await client.GetAsync($"{INVENTORY_SERVICE_API_BASE}/products");
             if(res2.IsSuccessStatusCode){
                 var result = res2.Content.ReadAsStringAsync().Result;
                 products = JsonConvert.DeserializeObject<List<Product>>(result);
